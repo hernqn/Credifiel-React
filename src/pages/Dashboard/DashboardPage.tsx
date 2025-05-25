3a2b2195a0ba3e376b2bd2e0a51d53aac0dbfb13
bananareverse <hernan272005@gmail.com>
2025-05-24 21:04:40 -0600

    z

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -5,7 +5,7 @@ import DashboardPrincipal from '../../components/Dashboard/Dashboard';
 const DashboardPage = () => {
     return (
         <div className="dashboard-page">
-            <h1 className="dashboard-page-title">Panel de Control</h1>
+            <h1 className="dashboard-page-title">Panel de Controlz</h1>
             <DashboardPrincipal />
         </div>
     );
